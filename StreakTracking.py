34e25e706a45dd8d93763a3ea0373ef601188217
alexisbernt <129408149+alexisbernt@users.noreply.github.com>
2023-11-01 10:59:10 -0500

    Update StreakTracking.py

--- StreakTracking.py
+++ StreakTracking.py
@@ -47,7 +47,34 @@ A) Give a direct implementation of the num children method within the class Bina
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 class BinaryTree(Tree):
 
-    def 
+    def left(self, p):
+        # return the position representing p's left child
+    raise NonImplementedError('must be implemented by subclass')
+
+    def right(self, p):
+        # return a Position representing p's right child
+        # return None if p does not have a right child
+    raise NonImplementedError('must be implemented by subclass')
+
+# -------- Create the methods for this class - ---------
+
+    def sibling(self, p):
+        # return a Position representing p's sibling (or None if no sibling)
+        parent = self.parent(p)
+        if parent is None:
+            return None
+        else:
+            if p == self.left(parent):
+                return self.right(parent)
+            else:
+                return self.left(parent)
+
+    def children(self, p):
+        # generate an iteration of Positions representing p's children
+        if self.left(p) is not None:
+            yield self.left(p)
+        if self.right(p) is not None:
+            yield self.right(p) 
 
 B) Implement the binary tree ADT using the array-based representation.
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
